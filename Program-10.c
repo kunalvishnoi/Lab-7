01c81dc32a77602db43d1fde3944cc326df5f0e4
kunalvishnoi <vishnoi.kunal0@gmail.com>
2017-08-16 22:49:12 +0530

    Binary Search n bibble n insertion sort

--- Program-10.c
+++ Program-10.c
@@ -1,27 +1,38 @@
 #include <stdio.h>
-
+ 
 void main()
 {
-    int temp,n,arr[100],i,j;
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    scanf("%d",&arr[i]);
-    for(i=0;i<(n-1);i++)
-    {
-        for(j=0;j<n-i-1;j++)
-        {
-        if(arr[j]>arr[j+1])
-{
-        temp=arr[j];
-        arr[j]=arr[j+1];
-        arr[j+1]=temp;
-        }
-    }
-    }
-        for(i=0;i<n;i++)
-        printf("%d",arr[i]);
-    }
-  
-
-
-
+   int i, first, last, middle, n, key, arr[100];
+ 
+   printf("Enter number of elements");
+   scanf("%d",&n);
+ 
+   printf("Enter  integers");
+ 
+   for (i= 0; i < n; i++)
+      scanf("%d",&arr[i]);
+ 
+   printf("Enter value to find");
+   scanf("%d", &key);
+ 
+   first = 0;
+   last = n - 1;
+   middle = (first+last)/2;
+ 
+   while (first <= last) {
+      if (arr[middle] < key)
+         first = middle + 1;    
+      else if (array[middle] == search) {
+         printf("%d found at location" middle+1);
+         break;
+      }
+      else
+         last = middle - 1;
+ 
+      middle = (first + last)/2;
+   }
+   if (first > last)
+      printf("Not found! ");
+ 
+   
+}
--- Program-11.c
+++ Program-11.c
@@ -1,35 +1,27 @@
 #include <stdio.h>
- 
-int main()
+
+void main()
 {
-  int n, array[1000], c, d, temp;
- 
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
- 
-  printf("Enter %d integers\n", n);
- 
-  for (c = 0; c < n; c++) {
-    scanf("%d", &array[c]);
-  }
- 
-  for (c = 1 ; c <= n - 1; c++) {
-    d = c;
- 
-    while ( d > 0 && array[d] < array[d-1]) {
-      temp = array[d];
-      array[d] = array[d-1];
-      array[d-1] = t;
- 
-      d--;
+    int temp,n,arr[100],i,j;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+    for(i=0;i<(n-1);i++)
+    {
+        for(j=0;j<n-i-1;j++)
+        {
+        if(arr[j]>arr[j+1])
+{
+        temp=arr[j];
+        arr[j]=arr[j+1];
+        arr[j+1]=temp;
+        }
     }
-  }
- 
-  printf("Sorted list in ascending order:\n");
- 
-  for (c = 0; c <= n - 1; c++) {
-    printf("%d\n", array[c]);
-  }
- 
-  return 0;
-}
+    }
+        for(i=0;i<n;i++)
+        printf("%d",arr[i]);
+    }
+  
+
+
+
